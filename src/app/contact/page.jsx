390a2009f91e447287fa618986e05a296538c9cd
jo33200 <jo33200@gmail.com>
2024-12-04 10:27:27 +0100

    Add Contact page component with ContactForm and ContactInfo sections

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,17 @@
+import ContactForm from "@/components/common/ContactForm";
+import ContactInfo from "@/components/common/ContactInfo";
+
+const Contact = () => {
+  return (
+    <div className="my-16 flex h-auto w-full flex-col items-center justify-center gap-10 md:my-0 lg:min-h-screen lg:items-start lg:flex-row lg:justify-around">
+      <section className="mt-5 w-full">
+        <ContactInfo />
+      </section>
+      <section className="mt-5 w-full">
+        <ContactForm />
+      </section>
+    </div>
+  );
+};
+
+export default Contact;
