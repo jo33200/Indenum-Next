b2c7a6e303f8784514ec9f5fb5ea2f50cb80812b
jo33200 <jo33200@gmail.com>
2025-01-02 11:18:00 +0100

    docs: legal information

--- src/app/details/page.jsx
+++ src/app/details/page.jsx
@@ -1,11 +1,433 @@
-
-
 const Details = () => {
-    return (
-        <div>
-            <h1>Details Page</h1>
-        </div>
-    );
+  return (
+    <section className="mx-5 max-w-[850px] pt-24">
+      <header>
+        <h1 className="mb-4 text-2xl font-bold">MENTIONS LÉGALES</h1>
+      </header>
+
+      <article className="mb-8">
+        <header>
+          <h2 className="mb-2 text-xl font-semibold">DÉFINITIONS</h2>
+        </header>
+        <p className="mb-4">
+          Client : Tout professionnel ou personne physique capable au sens des
+          articles 1123 et suivants du Code civil, ou personne morale, qui
+          visite le Site objet des présentes conditions générales.
+        </p>
+        <p className="mb-4">
+          Prestations et Services : https://www.indenum.fr/ met à disposition
+          des Clients :
+        </p>
+        <p className="mb-4">
+          Contenu : Ensemble des éléments constituants l’information présente
+          sur le Site, notamment textes – images – vidéos.
+        </p>
+        <p className="mb-4">
+          Informations clients : Ci-après dénommé « Information (s) » qui
+          correspondent à l’ensemble des données personnelles susceptibles
+          d’être détenues par https://www.indenum.fr/ pour la gestion de votre
+          compte, de la gestion de la relation client et à des fins d’analyses
+          et de statistiques.
+        </p>
+        <p className="mb-4">
+          Utilisateur : Internaute se connectant, utilisant le site susnommé.
+        </p>
+        <p className="mb-4">
+          Informations personnelles : « Les informations qui permettent, sous
+          quelque forme que ce soit, directement ou non, l'identification des
+          personnes physiques auxquelles elles s'appliquent » (article 4 de la
+          loi n° 78-17 du 6 janvier 1978).
+        </p>
+        <p className="mb-4">
+          Les termes « données à caractère personnel », « personne concernée »,
+          « sous-traitant » et « données sensibles » ont le sens défini par le
+          Règlement Général sur la Protection des Données (RGPD : n° 2016-679).
+        </p>
+      </article>
+
+      <article className="mb-8">
+        <header>
+          <h2 className="mb-2 text-xl font-semibold">
+            1. PRÉSENTATION DU SITE INTERNET
+          </h2>
+        </header>
+        <p className="mb-4">
+          En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la
+          confiance dans l'économie numérique, il est précisé aux utilisateurs
+          du site internet https://www.indenum.fr/ l'identité des différents
+          intervenants dans le cadre de sa réalisation et de son suivi :
+        </p>
+        <ul className="mb-4 list-disc pl-5">
+          <li>Propriétaire : Anthony RUIZ</li>
+          <li>Statut : Auto-entrepreneur (ou micro-entrepreneur)</li>
+          <li>Identifiant SIRET : 891 370 280 00010</li>
+          <li>
+            Responsable publication : Anthony RUIZ –{" "}
+            <a
+              href="mailto:indenum@outlook.com"
+              className="text-blue-600 underline"
+            >
+              indenum@outlook.com
+            </a>
+          </li>
+          <li>
+            Webmaster : Anthony RUIZ –{" "}
+            <a
+              href="mailto:indenum@outlook.com"
+              className="text-blue-600 underline"
+            >
+              indenum@outlook.com
+            </a>
+          </li>
+          <li>
+            Délégué à la protection des données : Anthony RUIZ –{" "}
+            <a
+              href="mailto:indenum@outlook.com"
+              className="text-blue-600 underline"
+            >
+              indenum@outlook.com
+            </a>
+          </li>
+        </ul>
+        <p className="text-sm italic">
+          Ce modèle de mentions légales est proposé par le générateur gratuit de
+          mentions légales pour un site internet.
+        </p>
+      </article>
+
+      <article className="mb-8">
+        <header>
+          <h2 className="mb-2 text-xl font-semibold">
+            2. CONDITIONS GÉNÉRALES D’UTILISATION DU SITE ET DES SERVICES
+            PROPOSÉS
+          </h2>
+        </header>
+        <p className="mb-4">
+          Le Site constitue une œuvre de l’esprit protégée par les dispositions
+          du Code de la Propriété Intellectuelle et des Réglementations
+          Internationales applicables. Le Client ne peut en aucune manière
+          réutiliser, céder ou exploiter pour son propre compte tout ou partie
+          des éléments ou travaux du Site.
+        </p>
+        <p className="mb-4">
+          L’utilisation du site https://www.indenum.fr/ implique l’acceptation
+          pleine et entière des conditions générales d’utilisation ci-après
+          décrites. Ces conditions d’utilisation sont susceptibles d’être
+          modifiées ou complétées à tout moment, les utilisateurs du site
+          https://www.indenum.fr/ sont donc invités à les consulter de manière
+          régulière.
+        </p>
+        <p className="mb-4">
+          Ce site internet est normalement accessible à tout moment aux
+          utilisateurs. Une interruption pour raison de maintenance technique
+          peut être toutefois décidée par https://www.indenum.fr/, qui
+          s’efforcera alors de communiquer préalablement aux utilisateurs les
+          dates et heures de l’intervention.
+        </p>
+        <p className="mb-4">
+          Le site web https://www.indenum.fr/ est mis à jour régulièrement par
+          https://www.indenum.fr/ responsable. De la même façon, les mentions
+          légales peuvent être modifiées à tout moment : elles s’imposent
+          néanmoins à l’utilisateur qui est invité à s’y référer le plus souvent
+          possible afin d’en prendre connaissance.
+        </p>
+      </article>
+
+      <article className="mb-8">
+        <header>
+          <h2 className="mb-2 text-xl font-semibold">
+            3. DESCRIPTION DES SERVICES FOURNIS
+          </h2>
+        </header>
+        <p className="mb-4">
+          Le site internet https://www.indenum.fr/ a pour objet de fournir une
+          information concernant l’ensemble des activités de la société.
+          https://www.indenum.fr/ s’efforce de fournir sur le site
+          https://www.indenum.fr/ des informations aussi précises que possible.
+          Toutefois, il ne pourra être tenu responsable des oublis, des
+          inexactitudes et des carences dans la mise à jour, qu’elles soient de
+          son fait ou du fait des tiers partenaires qui lui fournissent ces
+          informations.
+        </p>
+        <p className="mb-4">
+          Toutes les informations indiquées sur le site https://www.indenum.fr/
+          sont données à titre indicatif, et sont susceptibles d’évoluer. Par
+          ailleurs, les renseignements figurant sur le site
+          https://www.indenum.fr/ ne sont pas exhaustifs. Ils sont donnés sous
+          réserve de modifications ayant été apportées depuis leur mise en
+          ligne.
+        </p>
+      </article>
+
+      <article className="mb-8">
+        <header>
+          <h2 className="mb-2 text-xl font-semibold">
+            4. LIMITATIONS CONTRACTUELLES SUR LES DONNÉES TECHNIQUES
+          </h2>
+        </header>
+        <p className="mb-4">
+          Le site utilise la technologie JavaScript. Le site Internet ne pourra
+          être tenu responsable de dommages matériels liés à l’utilisation du
+          site. De plus, l’utilisateur du site s’engage à accéder au site en
+          utilisant un matériel récent, ne contenant pas de virus et avec un
+          navigateur de dernière génération mis-à-jour.
+        </p>
+        <p className="mb-4">
+          Le site https://www.indenum.fr/ est hébergé chez un prestataire sur le
+          territoire de l’Union Européenne conformément aux dispositions du
+          Règlement Général sur la Protection des Données (RGPD : n° 2016-679).
+        </p>
+        <p className="mb-4">
+          L’objectif est d’apporter une prestation qui assure le meilleur taux
+          d’accessibilité. L’hébergeur assure la continuité de son service 24
+          heures sur 24, tous les jours de l’année. Il se réserve néanmoins la
+          possibilité d’interrompre le service d’hébergement pour les durées les
+          plus courtes possibles notamment à des fins de maintenance,
+          d’amélioration de ses infrastructures, de défaillance de ses
+          infrastructures ou si les Prestations et Services génèrent un trafic
+          réputé anormal.
+        </p>
+        <p className="mb-4">
+          https://www.indenum.fr/ et l’hébergeur ne pourront être tenus
+          responsables en cas de dysfonctionnement du réseau Internet, des
+          lignes téléphoniques ou du matériel informatique et de téléphonie liée
+          notamment à l’encombrement du réseau empêchant l’accès au serveur.
+        </p>
+      </article>
+
+      <article className="mb-8">
+        <header>
+          <h2 className="mb-2 text-xl font-semibold">
+            5. PROPRIÉTÉ INTELLECTUELLE ET CONTREFAÇONS
+          </h2>
+        </header>
+        <p className="mb-4">
+          https://www.indenum.fr/ est propriétaire des droits de propriété
+          intellectuelle et détient les droits d’usage sur tous les éléments
+          accessibles sur le site internet, notamment les textes, images,
+          graphismes, logos, vidéos, icônes et sons.
+        </p>
+        <p className="mb-4">
+          Toute reproduction, représentation, modification, publication,
+          adaptation de tout ou partie des éléments du site, quel que soit le
+          moyen ou le procédé utilisé, est interdite, sauf autorisation écrite
+          préalable de : https://www.indenum.fr/.
+        </p>
+        <p className="mb-4">
+          Toute exploitation non autorisée du site ou de quelconque des éléments
+          qu’il contient sera considérée comme constitutive d’une contrefaçon et
+          poursuivie conformément aux dispositions des articles L.335-2 et
+          suivants du Code de Propriété Intellectuelle.
+        </p>
+      </article>
+
+      <article className="mb-8">
+        <header>
+          <h2 className="mb-2 text-xl font-semibold">
+            6. LIMITATIONS DE RESPONSABILITÉ
+          </h2>
+        </header>
+        <p className="mb-4">
+          https://www.indenum.fr/ agit en tant qu’éditeur du site.
+          https://www.indenum.fr/ est responsable de la qualité et de la
+          véracité du Contenu qu’il publie.
+        </p>
+        <p className="mb-4">
+          https://www.indenum.fr/ ne pourra être tenu responsable des dommages
+          directs et indirects causés au matériel de l’utilisateur, lors de
+          l’accès au site internet https://www.indenum.fr/, et résultant soit de
+          l’utilisation d’un matériel ne répondant pas aux spécifications
+          indiquées au point 4, soit de l’apparition d’un bug ou d’une
+          incompatibilité.
+        </p>
+        <p className="mb-4">
+          https://www.indenum.fr/ ne pourra également être tenu responsable des
+          dommages indirects (tels par exemple qu’une perte de marché ou perte
+          d’une chance) consécutifs à l’utilisation du site
+          https://www.indenum.fr/.
+        </p>
+        <p className="mb-4">
+          Des espaces interactifs (possibilité de poser des questions dans
+          l’espace contact) sont à la disposition des utilisateurs.
+          https://www.indenum.fr/ se réserve le droit de supprimer, sans mise en
+          demeure préalable, tout contenu déposé dans cet espace qui
+          contreviendrait à la législation applicable en France, en particulier
+          aux dispositions relatives à la protection des données.
+        </p>
+      </article>
+
+      <article className="mb-8">
+        <header>
+          <h2 className="mb-2 text-xl font-semibold">
+            7. GESTION DES DONNÉES PERSONNELLES
+          </h2>
+        </header>
+        <p className="mb-4">
+          Le Client est informé des réglementations concernant la communication
+          marketing, la loi du 21 Juin 2014 pour la confiance dans l’Économie
+          Numérique, la Loi Informatique et Liberté du 06 Août 2004 ainsi que du
+          Règlement Général sur la Protection des Données (RGPD : n° 2016-679).
+        </p>
+
+        <article className="mb-6">
+          <header>
+            <h3 className="mb-2 text-lg font-semibold">
+              7.1 Responsables de la collecte des données personnelles
+            </h3>
+          </header>
+          <p className="mb-4">
+            Pour les Données Personnelles collectées dans le cadre de la
+            création du compte personnel de l’Utilisateur et de sa navigation
+            sur le Site, le responsable du traitement des Données Personnelles
+            est : Anthony RUIZ.
+          </p>
+          <p className="mb-4">
+            En tant que responsable du traitement des données qu’il collecte,
+            https://www.indenum.fr/ s’engage à respecter le cadre des
+            dispositions légales en vigueur. Il lui appartient notamment
+            d’établir les finalités de ses traitements, de fournir à ses
+            prospects et clients une information complète sur le traitement de
+            leurs données personnelles et de maintenir un registre des
+            traitements conforme à la réalité.
+          </p>
+        </article>
+
+        <article className="mb-6">
+          <header>
+            <h3 className="mb-2 text-lg font-semibold">
+              7.2 Finalité des données collectées
+            </h3>
+          </header>
+          <p className="mb-4">
+            https://www.indenum.fr/ est susceptible de traiter tout ou partie
+            des données pour les finalités suivantes :
+          </p>
+          <ul className="mb-4 list-disc pl-5">
+            <li>
+              Permettre la navigation sur le Site et la gestion des services
+              commandés (données de connexion, historique des commandes, etc.).
+            </li>
+            <li>
+              Prévenir et lutter contre la fraude informatique (spamming,
+              hacking…).
+            </li>
+            <li>Améliorer la navigation sur le Site.</li>
+            <li>Mener des enquêtes de satisfaction facultatives.</li>
+            <li>Conduire des campagnes de communication (SMS, email).</li>
+          </ul>
+          <p className="mb-4">
+            Les données personnelles ne sont pas commercialisées et sont
+            utilisées uniquement par nécessité ou à des fins statistiques et
+            d’analyse.
+          </p>
+        </article>
+
+        <article className="mb-6">
+          <header>
+            <h3 className="mb-2 text-lg font-semibold">
+              7.3 Droits d’accès, de rectification et d’opposition
+            </h3>
+          </header>
+          <p className="mb-4">
+            Conformément à la réglementation européenne, les Utilisateurs
+            disposent des droits suivants :
+          </p>
+          <ul className="mb-4 list-disc pl-5">
+            <li>
+              Droit d’accès, de rectification et de mise à jour de leurs
+              données.
+            </li>
+            <li>Droit à l’effacement des données personnelles.</li>
+            <li>Droit de retirer leur consentement à tout moment.</li>
+            <li>Droit à la limitation du traitement de leurs données.</li>
+            <li>Droit à la portabilité des données fournies.</li>
+            <li>Droit de définir le sort de leurs données après leur décès.</li>
+          </ul>
+          <p className="mb-4">
+            Pour exercer ces droits, l’Utilisateur peut contacter Anthony RUIZ
+            par écrit à l’adresse suivante :
+          </p>
+          <address className="mb-4">
+            Anthony RUIZ
+            <br />
+            Résidence Les Cèdres Bleus, Bât A – 350 avenue de la Libération
+            Charles de Gaulle
+            <br />
+            33110 Le Bouscat.
+          </address>
+          <p className="mb-4">
+            Les demandes doivent être accompagnées d’une copie d’une pièce
+            d’identité valide pour vérification.
+          </p>
+        </article>
+
+        <article className="mb-6">
+          <header>
+            <h3 className="mb-2 text-lg font-semibold">
+              7.4 Non-communication des données personnelles
+            </h3>
+          </header>
+          <p className="mb-4">
+            https://www.indenum.fr/ s’interdit de traiter, héberger ou
+            transférer les données collectées hors de l’Union Européenne sans en
+            informer au préalable le client. Toutes les précautions sont prises
+            pour préserver la sécurité et la confidentialité des informations
+            collectées.
+          </p>
+          <p className="mb-4">
+            En cas d’incident impactant les données personnelles,
+            https://www.indenum.fr/ s’engage à informer les utilisateurs
+            concernés et à prendre les mesures correctives nécessaires.
+          </p>
+        </article>
+      </article>
+
+      <article className="mb-8">
+        <header>
+          <h2 className="mb-2 text-xl font-semibold">
+            8. NOTIFICATION D’INCIDENT
+          </h2>
+        </header>
+        <p className="mb-4">
+          En cas de brèche de sécurité, les utilisateurs seront avertis pour
+          prendre les mesures appropriées. https://www.indenum.fr/ respecte ses
+          obligations légales en matière de sécurité et de confidentialité.
+        </p>
+      </article>
+
+      <article className="mb-8">
+        <header>
+          <h2 className="mb-2 text-xl font-semibold">
+            9. LIENS HYPERTEXTES « COOKIES » ET BALISES (“TAGS”) INTERNET
+          </h2>
+        </header>
+        <p className="mb-4">
+          Le site peut contenir des liens vers d’autres sites. Cependant,
+          https://www.indenum.fr/ décline toute responsabilité concernant le
+          contenu ou l’utilisation de ces sites tiers.
+        </p>
+        <p className="mb-4">
+          Sauf désactivation par l’utilisateur, le site utilise des cookies pour
+          améliorer la navigation. Ces cookies peuvent être désactivés via les
+          paramètres du navigateur.
+        </p>
+      </article>
+
+      <article className="mb-8">
+        <header>
+          <h2 className="mb-2 text-xl font-semibold">
+            10. DROIT APPLICABLE ET ATTRIBUTION DE JURIDICTION
+          </h2>
+        </header>
+        <p className="mb-4">
+          Tout litige en relation avec l’utilisation du site
+          https://www.indenum.fr/ est soumis au droit français. En dehors des
+          cas où la loi ne le permet pas, il est fait attribution exclusive de
+          juridiction aux tribunaux compétents de Bordeaux.
+        </p>
+      </article>
+    </section>
+  );
 };
 
 export default Details;
