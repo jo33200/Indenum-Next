934d8c448ef6ee11eb5641804ce2eb9698315d8e
jo33200 <jo33200@gmail.com>
2025-01-02 11:28:09 +0100

    Add detailed Privacy Policy content and improve layout for better readability

--- src/app/privacy/page.jsx
+++ src/app/privacy/page.jsx
@@ -1,11 +1,222 @@
-
-
 const Privacy = () => {
   return (
-    <div>
-      <h1>Privacy</h1>
-    </div>
+    <section className="mx-5 max-w-[850px] pt-24">
+      <header>
+        <h1 className="mb-4 text-2xl font-bold">
+          POLITIQUE DE CONFIDENTIALITÉ
+        </h1>
+      </header>
+
+      <article className="mb-8">
+        <header>
+          <h2 className="mb-2 text-xl font-semibold">
+            ARTICLE 1 – RENSEIGNEMENTS PERSONNELS RECUEILLIS
+          </h2>
+        </header>
+        <p>
+          Lorsque vous effectuez un achat sur notre boutique, dans le cadre de
+          notre processus d’achat et de vente, nous recueillons les
+          renseignements personnels que vous nous fournissez, tels que votre
+          nom, votre adresse et votre adresse e-mail.
+        </p>
+        <p>
+          Lorsque vous naviguez sur notre boutique, nous recevons également
+          automatiquement l’adresse de protocole Internet (adresse IP) de votre
+          ordinateur, qui nous permet d’obtenir plus de détails au sujet du
+          navigateur et du système d’exploitation que vous utilisez.
+        </p>
+        <p>
+          <strong>Marketing par e-mail (le cas échéant) :</strong> Avec votre
+          permission, nous pourrions vous envoyer des e-mails au sujet de notre
+          boutique, de nouveaux produits et d’autres mises à jour.
+        </p>
+      </article>
+
+      <article className="mb-8">
+        <header>
+          <h2 className="mb-2 text-xl font-semibold">
+            ARTICLE 2 – CONSENTEMENT
+          </h2>
+        </header>
+        <h3 className="text-lg font-semibold">
+          Comment obtenez-vous mon consentement ?
+        </h3>
+        <p>
+          Lorsque vous nous fournissez vos renseignements personnels pour
+          conclure une transaction, vérifier votre carte de crédit, passer une
+          commande, planifier une livraison ou retourner un achat, nous
+          présumons que vous consentez à ce que nous recueillions vos
+          renseignements et à ce que nous les utilisions à cette fin uniquement.
+        </p>
+        <p>
+          Si nous vous demandons de nous fournir vos renseignements personnels
+          pour une autre raison, à des fins de marketing par exemple, nous vous
+          demanderons directement votre consentement explicite, ou nous vous
+          donnerons la possibilité de refuser.
+        </p>
+        <h3 className="text-lg font-semibold">
+          Comment puis-je retirer mon consentement ?
+        </h3>
+        <p>
+          Si après nous avoir donné votre consentement, vous changez d’avis et
+          ne consentez plus à ce que nous puissions vous contacter, recueillir
+          vos renseignements ou les divulguer, vous pouvez nous en aviser en
+          nous contactant à :{" "}
+          <a href="mailto:indenum@outlook.com">indenum@outlook.com</a>.
+        </p>
+      </article>
+
+      <article className="mb-8">
+        <header>
+          <h2 className="mb-2 text-xl font-semibold">
+            ARTICLE 3 – DIVULGATION
+          </h2>
+        </header>
+        <p>
+          Nous pouvons divulguer vos renseignements personnels si la loi nous
+          oblige à le faire ou si vous violez nos Conditions Générales de Vente
+          et d’Utilisation.
+        </p>
+      </article>
+
+      <article className="mb-8">
+        <header>
+          <h2 className="mb-2 text-xl font-semibold">ARTICLE 4 – GOOGLE</h2>
+        </header>
+        <p>
+          Notre boutique est hébergée sur Google Inc. Ils nous fournissent la
+          plate-forme web en ligne qui nous permet de vous vendre nos services
+          et produits. Vos données sont stockées dans le système de stockage de
+          données et les bases de données de Google, et dans l’application
+          générale de Google. Vos données sont conservées sur un serveur
+          sécurisé protégé par un pare-feu.
+        </p>
+        <p>
+          <strong>Paiement :</strong> Si vous réalisez votre achat par le biais
+          de notre formulaire de commande, vos renseignements personnels seront
+          traités par le biais de Google Sheet. Pour plus d’informations,
+          veuillez consulter les conditions d’utilisation de Google ici ou leur
+          Politique de Confidentialité ici.
+        </p>
+      </article>
+
+      <article className="mb-8">
+        <header>
+          <h2 className="mb-2 text-xl font-semibold">
+            ARTICLE 5 – SERVICES FOURNIS PAR DES TIERS
+          </h2>
+        </header>
+        <p>
+          De manière générale, les fournisseurs tiers que nous utilisons vont
+          uniquement recueillir, utiliser et divulguer vos renseignements dans
+          la mesure du nécessaire pour pouvoir réaliser les services qu’ils nous
+          fournissent.
+        </p>
+        <p>
+          Cependant, certains tiers fournisseurs, comme les passerelles de
+          paiement, possèdent leurs propres politiques de confidentialité. Nous
+          vous recommandons de lire attentivement leurs politiques de
+          confidentialité pour comprendre comment ils traiteront vos
+          renseignements personnels.
+        </p>
+        <p>
+          Une fois que vous quittez le site de notre boutique ou que vous êtes
+          redirigé vers le site d’un tiers, vous n’êtes plus régi par cette
+          Politique de Confidentialité.
+        </p>
+      </article>
+
+      <article className="mb-8">
+        <header>
+          <h2 className="mb-2 text-xl font-semibold">ARTICLE 6 – SÉCURITÉ</h2>
+        </header>
+        <p>
+          Pour protéger vos données personnelles, nous prenons des précautions
+          raisonnables et suivons les meilleures pratiques de l’industrie pour
+          nous assurer qu’elles ne soient pas perdues, détournées, consultées,
+          divulguées, modifiées ou détruites de manière inappropriée.
+        </p>
+        <p>
+          Si vous nous fournissez vos informations de carte de crédit, elles
+          seront chiffrées par le biais du protocole SSL et conservées avec un
+          chiffrement AES-256. Bien qu’aucune méthode de transmission sur
+          Internet ou de stockage électronique ne soit sûre à 100 %, nous
+          suivons toutes les exigences de la norme PCI-DSS.
+        </p>
+      </article>
+
+      <article className="mb-8">
+        <header>
+          <h2 className="mb-2 text-xl font-semibold">
+            ARTICLE 7 – ÂGE DE CONSENTEMENT
+          </h2>
+        </header>
+        <p>
+          En utilisant ce site, vous déclarez que vous avez au moins l’âge de la
+          majorité dans votre État ou province de résidence, et que vous nous
+          avez donné votre consentement pour permettre à toute personne d’âge
+          mineur à votre charge d’utiliser ce site web.
+        </p>
+      </article>
+
+      <article className="mb-8">
+        <header>
+          <h2 className="mb-2 text-xl font-semibold">
+            ARTICLE 8 – MODIFICATIONS APPORTÉES À LA PRÉSENTE POLITIQUE DE
+            CONFIDENTIALITÉ
+          </h2>
+        </header>
+        <p>
+          Nous nous réservons le droit de modifier cette politique de
+          confidentialité à tout moment. Les changements prendront effet
+          immédiatement après leur publication sur le site. Si des modifications
+          importantes sont apportées, nous vous en informerons ici.
+        </p>
+      </article>
+
+      <article className="mb-8">
+        <header>
+          <h2 className="mb-2 text-xl font-semibold">
+            ARTICLE 9 – DONNÉES PERSONNELLES
+          </h2>
+        </header>
+        <p>
+          La présente politique vise à vous informer de nos pratiques concernant
+          la collecte, l’utilisation et le partage des informations que vous
+          êtes amenés à nous communiquer par le biais de nos services et
+          produits.
+        </p>
+        <p>
+          Toutes vos données personnelles sont protégées conformément aux lois
+          en vigueur. Pour toute question, contactez-nous à :{" "}
+          <a
+            href="mailto:indenum@outlook.com"
+            className="text-blue-600 underline"
+          >
+            indenum@outlook.com
+          </a>
+          .
+        </p>
+      </article>
+
+      <article className="mb-8">
+        <header>
+          <h3 className="text-lg font-semibold">QUESTIONS ET COORDONNÉES</h3>
+        </header>
+        <p>
+          Pour toute demande concernant vos données, veuillez contacter notre
+          agent responsable des normes de confidentialité à :{" "}
+          <a
+            href="mailto:indenum@outlook.com"
+            className="text-blue-600 underline"
+          >
+            indenum@outlook.com
+          </a>
+          .
+        </p>
+      </article>
+    </section>
   );
-}
+};
 
 export default Privacy;
